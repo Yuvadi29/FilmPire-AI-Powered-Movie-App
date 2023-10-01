5ade227007b828829ea7d099f51dedd1993436ce
Yuvadi29 <adityatrivedi612@gmail.com>
2023-10-01 11:49:01 +0530

    Routing and Component Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import MovieInformation from './MovieInformation/MovieInformation';
+import Movies from './Movies/Movies';
+import Actors from './Actors/Actors';
+import Profile from './Profile/Profile';
+import Navbar from './Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <h1><Movies /></h1>
+          </Route>
+          <Route exact path="/movie/:id">
+            <h1><MovieInformation /></h1>
+          </Route>
+          <Route exact path="/actors/:id">
+            <h1><Actors /></h1>
+          </Route>
+          <Route exact path="/profile/:id">
+            <h1><Profile /></h1>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
