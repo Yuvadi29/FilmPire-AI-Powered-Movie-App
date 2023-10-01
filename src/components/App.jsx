f964f718205901cd853cbfa1efa2c205bcd6676a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-10-01 11:59:14 +0530

    Basic Styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,18 @@ import Movies from './Movies/Movies';
 import Actors from './Actors/Actors';
 import Profile from './Profile/Profile';
 import Navbar from './Navbar/Navbar';
+import useStyles from './styles';
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Switch>
           <Route exact path="/">
             <h1><Movies /></h1>
