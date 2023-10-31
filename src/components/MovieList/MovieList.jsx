2c25a45f0cd9ebc83a7292a437ed074c24350fb9
Yuvadi29 <adityatrivedi612@gmail.com>
2023-10-31 16:43:04 +0530

    Movies Rendered and Displayed on Frontend

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,19 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import useStyles from './styles';
+import Movie from '../Movie/Movie';
+
+const MovieList = ({ movies }) => {
+    const classes = useStyles();
+    console.log('Movie List')
+
+    return (
+        <Grid container className={classes.moviesContainer}>
+            {movies.results.map((movie, index) => (
+                <Movie key={index} movie={movie} index={index} />
+            ))}
+        </Grid>
+    )
+}
+
+export default MovieList;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,16 +3,36 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 
 const Movies = () => {
 
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data);
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    )
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant='h4'>No Movies Match That Name
+          <br /> Kindly Search for Something Else</Typography>
+      </Box>
+    )
+  }
+
+  if(error) return "An Error Has Occured.";
 
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   )
 }
 
